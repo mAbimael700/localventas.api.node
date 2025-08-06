c6b0ad15480dbfca1f8fe6f7d0446cd6cae85d35
mAbimael <mabimael700@gmail.com>
2025-08-05 23:10:59 -0600

    Mejora: se agregó wget al Dockerfile para el healthcheck y se actualizó el comando de verificación de salud. Además, se añadió un mensaje de inicio del servidor en www.mjs.

--- bin/www.mjs
+++ bin/www.mjs
@@ -22,6 +22,10 @@ app.set('port', port);
 
 const server = createServer(app);
 
+server.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on port ${port}`);
+});
+
 /**
  * Listen on provided port, on all network interfaces.
  */
